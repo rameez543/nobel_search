1cb0a9a2dd7a95eb2ac0d374dde0ae67f1d571f4
rameez <rameez@ceegees.in>
2019-12-08 13:27:06 +0530

    improved searching

--- server/server.js
+++ server/server.js
@@ -3,12 +3,18 @@ const app = express()
 const cors = require('cors')
 const prizeJson = require('./prize.json')
 const personJson = require('./laurete.json')
+const port = process.env.PORT || 2000
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-function search(searchText){
-    let person = personJson.laureates.filter(item => (item.firstname.toUpperCase() === searchText.toUpperCase())||(item.surname&&item.surname.toUpperCase() === searchText.toUpperCase()))
+function search(searchText) {
+    let person = personJson.laureates.filter(item =>
+        (searchText.toUpperCase().includes(`${item.firstname} ${item.surname}`.toUpperCase())) ||
+        (searchText.toUpperCase().includes(item.firstname && item.firstname.toUpperCase())) ||
+        (searchText.toUpperCase().includes(item.surname && item.surname.toUpperCase()))
+
+    )
     let searchedResult = []
     let laureatesObject = []
     prizeJson.prizes.forEach(element => {
@@ -27,16 +33,16 @@ function search(searchText){
             field: "",
             shares: [],
             year: "",
-            motivation:""
-    
+            motivation: ""
+
         }
         for (let j of laureatesObject) {
             i.prizes.forEach(element => {
                 personObject.field = element.category
                 personObject.year = element.year
-                personObject.motivation =element.motivation
+                personObject.motivation = element.motivation
                 if (element.motivation === j.motivation && i.id !== j.id) {
-                    
+
                     personObject.shares.push(`${j.firstname} ${j.surname}`)
                 }
             })
@@ -49,17 +55,17 @@ function search(searchText){
 
 
 app.post('/getResult', function (req, res) {
-   const searchText = req.body.search
-   const result =search(searchText)
-   try{
-    if(result.length>0){
-       return res.json({success:true,result:result})
+    const searchText = req.body.search
+    const result = search(searchText)
+    try {
+        if (result.length > 0) {
+            return res.json({ success: true, result: result })
+        }
+        return res.json({ success: false, result: "sorry,no data found" })
+    } catch (ex) {
+        res.json({ error: ex })
     }
-    return res.json({success:false,result:"sorry,no data found"})
-   }catch(ex){
-    res.json({ error:ex })
-   }
-    
+
 })
 
-app.listen(2000)
+app.listen(port,()=>console.log(`server started in PORT ${port}`))
